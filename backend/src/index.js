dedb304193ac6bd6f1a1229e336a675bab3093f6
Akshay Rawat <155710637+Akshay-Rawat@users.noreply.github.com>
2025-01-03 01:49:52 +0530

    commit changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,54 +2,41 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-import path from "path";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// Middleware to set CSP header
-app.use((req, res, next) => {
-    res.setHeader(
-        'Content-Security-Policy',
-        "default-src 'none'; font-src 'self' https://fonts.gstatic.com; script-src 'self' https://x-chat-csjv.onrender.com; object-src 'none'; style-src 'self' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=';"
-    );
-    next();
-});
-
-// Middleware
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json());
 app.use(cookieParser());
-
-// CORS Configuration
 app.use(
-    cors({
-        origin: process.env.NODE_ENV === "production" ? "https://your-frontend-domain.com" : "http://localhost:5173",
-        credentials: true,
-    })
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
 );
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-// Server Start
 server.listen(PORT, () => {
-    console.log("server is running on PORT:" + PORT);
-    connectDB();
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
